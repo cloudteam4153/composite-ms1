33b8d689bd8a890b3437d960fd3839921112a8e9
Sanjay Kumar <sanjayvkumar31@gmail.com>
2025-12-11 19:48:57 -0500

    updated allow_origins middleware for specific sites

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ app = FastAPI(
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173", "https://storage.googleapis.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
